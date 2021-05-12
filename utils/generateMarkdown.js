98b4c178ab0f16577cc3d5b212a664868b8d6cec
EEErban <f.erban@outlook.com>
2021-05-12 22:58:09 +0100

    markdown editing

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -32,15 +32,15 @@ const generateMarkdown = (data) => {
   ${data.description}
   
   ## Table of Contents
-  
-    - [Description](#description)
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [License](#license)
-    - [Contributing](#contributing)
-    - [Tests](#tests)
-    - [Questions](#questions)
-  
+
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
   ## Installation
   ${data.installation}
   
