7a6b83e2d72534ac03c91741bd93af73ccd899df
Farah Erban <f-erban@Farah-Erbans-MacBook-Pro.local>
2021-05-12 17:20:40 +0100

    question variables added

--- index.js
+++ index.js
@@ -6,7 +6,54 @@ const generateReadme = require("./utils/generateMarkdown")
 const writeFileAsync = util.promisify(fs.writeFile)
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is your project title?: ",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Enter a short project description: ",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What is the installation process?: ",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Enter the usage of the project: ",
+        name: "usage"
+
+    },
+    {
+        type: "list",
+        message: "What licenses are used for the project?: ",
+        name: "licenses",
+        choices: [
+            "MIT License",
+            "Code Project Open License (CPOL)",
+            "Common Development and Distribution License (CDDL)",
+            "Microsoft Public License (Ms-PL)",
+            "Mozilla Public License 1.1 (MPL 1.1)",
+            "Common Public License Version 1.0 (CPL)",
+            "Eclipse Public License 1.0",
+            "Apache License, Version 2.0"
+        ]
+    },
+    {
+        type: "input",
+        message: "Enter contributing information: ",
+        name: "contribute"
+
+    }, {
+        type: "input",
+        message: "What tests you are running for your project?: ",
+        name: "tests"
+    }
+];
 
 // TODO: Create a function to write README file
 const writeToFile = (fileName, data) => {};
